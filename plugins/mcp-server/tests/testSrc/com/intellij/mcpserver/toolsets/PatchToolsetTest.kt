a124ca5ad82d7bffc1e93c76bd4895652ab0bad4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2026-02-25 14:39:58 +0100

    IJPL-236508 hide git_status and apply
    GitOrigin-RevId: 18e41ee58610352566f8d049f9f0135697a74414

--- plugins/mcp-server/tests/testSrc/com/intellij/mcpserver/toolsets/PatchToolsetTest.kt
+++ plugins/mcp-server/tests/testSrc/com/intellij/mcpserver/toolsets/PatchToolsetTest.kt
@@ -3,11 +3,13 @@
 package com.intellij.mcpserver.toolsets
 
 import com.intellij.mcpserver.McpToolsetTestBase
+import com.intellij.mcpserver.impl.DisallowListBasedMcpToolFilterProvider.Companion.ENABLE_APPLY_PATCH_TOOL_REGISTRY_KEY
 import com.intellij.mcpserver.toolsets.general.FileToolset
 import com.intellij.mcpserver.toolsets.general.PatchToolset
 import com.intellij.mcpserver.toolsets.general.TextToolset
 import com.intellij.openapi.application.readAndEdtWriteAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.testFramework.junit5.RegistryKey
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.JsonPrimitive
@@ -15,6 +17,7 @@ import kotlinx.serialization.json.buildJsonObject
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
+@RegistryKey(key = ENABLE_APPLY_PATCH_TOOL_REGISTRY_KEY, value = "true")
 class PatchToolsetTest : McpToolsetTestBase() {
   @Test
   fun apply_patch_adds_file() = runBlocking(Dispatchers.Default) {
